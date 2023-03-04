00911d9e91265875da688bcf6f2497ba8f705b13
Gil Duplissy <gilzero@hotmail.com>
2023-03-04 21:24:02 +0100

    con changes

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
  <link rel='stylesheet' type='text/css' href='style/connexion.css'>
 </head>
 <body>
-   <h1>CONNEXION</h1>
-   <form action="home.php" method="post" class="form">
-      <div class="div_form">
-   		<label for="login" class="form-label">Log name</label>
-   	   <input type="login" class="form-control" id="login" name="login" placeholder=" (/ 'x' \)" required> 
-   	</div>
-   	<div class="div_form">
-   		<label for="password" class="form-label">Password</label>
-   		<input type="password" class="form-control" id="password" name="password" placeholder="**********" required>
-   	</div>
-   	<div>
-   		<button type="submit" class="form-submit">Connect</button>	            
-   		<button type="reset" class="form-reset">Reset</button>	             
-      </div>
-   </form>	
+	<div>
+		<img src="images/Menuiz Man.png">
+		<h1>CONNEXION</h1>
+   	<form action="home.php" method="post" class="form">
+   	   <div class="div_form">
+   			<label for="login" class="form-label">Log name</label>
+   		   <input type="login" class="form-control" id="login" name="login" placeholder=" (/ 'x' \)" required> 
+   		</div>
+   		<div class="div_form">
+   			<label for="password" class="form-label">Password</label>
+   			<input type="password" class="form-control" id="password" name="password" placeholder="**********" required>
+   		</div>
+   		<div>
+   			<button type="submit" class="form-submit">Connect</button>	            
+   			<button type="reset" class="form-reset">Reset</button>	             
+   	   </div>
+   	</form>
+	<div>
 </body>
 </html>
