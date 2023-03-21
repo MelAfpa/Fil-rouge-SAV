9fd4c7077380f086d665774ae10e9b0e1ff861d3
MelAfpa <120370567+MelAfpa@users.noreply.github.com>
2023-03-21 11:46:35 +0100

    Delete SAV directory

--- SAV/View/view_home_sav.php
+++
@@ -1,7 +0,0 @@
-<?php ob_start(); ?>
-<?php $content= 'DEBUG view: SAV<br><br>'; ?>
-<?php $content.= ob_get_clean(); ?>
-<?php $content.= "<br><br> <strong> Welcome: ".$user['Log_name'] . " from " . $user['branche'] . "<strong>"; ?>
-
-<?php require_once('template.php'); ?>
-
--- SAV/View/view_ticket.php
+++
@@ -1,5 +0,0 @@
-<?php ob_start(); ?>
-<?php $content = 'DEBUG view: TICKET <br><br>'; ?>
-<?php $content .= ob_get_clean(); ?>
-
-<?php require_once('template.php'); ?>
